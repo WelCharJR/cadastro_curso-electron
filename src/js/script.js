43a615af200445123b1da5512c27eb76e220530d
Welington <welingtoncharnetjr@gmail.com>
2025-05-26 22:21:27 -0300

    cria

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+var form = document.getElementById('form-curso')
+
+var meusCursos = []
+
+form.addEventListener("submit", function(event){
+    event.preventDefault
+
+
+    var nomeDoCurso = document.getElementById('nome-curso').value
+    var cargaHoraria = document.getElementById('carga-horaria-curso').value
+
+    var objetoCurso = {
+        nome: nomeDoCurso,
+        cargaHorarias: cargaHoraria
+
+    }
+
+    meusCursos.push(objetoCurso)
+
+    console.log("Minha lista de cursos:", meusCursos)
+
+    meusCursos.forEach(curso => {
+        var minhaLi = document.createElement("li")
+
+        minhaLi.innerText = curso
+    })
+
+
+})
+
